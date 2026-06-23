896c69f9d4e1b4f33678c3f49871c2daf60821df
wbaamaral <wbaamaral@gmail.com>
2026-06-23 10:20:07 -0400

    fix(scripts): corrigir ordem dos wrappers para o parser do PowerShell

--- scripts/verificar-atualizacoes-hardware.ps1
+++ scripts/verificar-atualizacoes-hardware.ps1
@@ -1,5 +1,11 @@
 ﻿#!/usr/bin/env pwsh
 #requires -version 5.1
+[CmdletBinding()]
+param(
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$RemainingArgs
+)
+
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 [Console]::InputEncoding  = [System.Text.Encoding]::UTF8
 $OutputEncoding           = [System.Text.Encoding]::UTF8
@@ -7,11 +13,6 @@ $PSDefaultParameterValues['Out-File:Encoding']    = 'utf8'
 $PSDefaultParameterValues['Set-Content:Encoding'] = 'utf8'
 $PSDefaultParameterValues['Add-Content:Encoding'] = 'utf8'
 try { chcp 65001 | Out-Null } catch { }
-[CmdletBinding()]
-param(
-    [Parameter(ValueFromRemainingArguments = $true)]
-    [string[]]$RemainingArgs
-)
 
 $target = Join-Path $PSScriptRoot '../diagnostics/Verificar-Atualizacoes-Hardware.ps1'
 & $target @RemainingArgs
